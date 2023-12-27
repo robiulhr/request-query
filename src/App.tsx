4cda3dc840edda687563d2702de8f78b6bffaa33
robiul <rh22012001@gmail.com>
2023-12-27 12:47:17 +0600

    added the cancel api request functionality in the useAxios hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { useAxios } from "./useAxios";
+import { PostDataType } from "./type";
+
 function App() {
-  const [data, setData] = useState();
-  const { response, error, loading } = useAxios({
+  const [data, setData] = useState<PostDataType | null>();
+  const { response, error, loading, cancelRequest } = useAxios<PostDataType>({
+    timeout: 3000,
     method: "post",
     url: "/posts",
+    timeoutErrorMessage: "Timeout Error.",
     headers: { accept: "*/*" },
     data: {
       userId: 1,
@@ -14,11 +18,13 @@ function App() {
       body: "Sample text",
     },
   });
+
   useEffect(() => {
     if (response !== null) {
       setData(response);
     }
-  }, [response]);
+    console.log(error);
+  }, [response, error]);
   return (
     <>
       <h1>Here is the response below:</h1>
--- src/useAxios.tsx
+++ src/useAxios.tsx
@@ -1,22 +1,71 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import environment from "./environments";
 
+if (process.env.NODE_ENV === "production") console.log = () => {};
+
 axios.defaults.baseURL = environment.baseUrl;
 
-export const useAxios = <T,>(axiosParams: AxiosRequestConfig) => {
+type useAxiosReturnType<T> = {
+  response: T | null;
+  error: AxiosError | unknown;
+  loading: boolean;
+  cancelRequest?: (timeoutMs?: number) => void;
+};
+type useAxiosType = <T>(axiosParams: AxiosRequestConfig) => useAxiosReturnType<T>;
+
+export const useAxios: useAxiosType = <T,>(axiosParams: AxiosRequestConfig) => {
   const [response, setResponse] = useState<T | null>(null);
   const [error, setError] = useState<AxiosError | unknown>();
   const [loading, setLoading] = useState(true);
+  const abortControllerRef = useRef(new AbortController());
+  const setTimeOutRef = useRef<number | null>(null);
+  const cancelRequest = useCallback(
+    (timeoutMs?: number) => {
+      if (setTimeOutRef.current) {
+        clearTimeout(setTimeOutRef.current);
+        setTimeOutRef.current = null;
+      }
+      setTimeOutRef.current = setTimeout(() => {
+        abortControllerRef.current.abort();
+        clearTimeout(setTimeOutRef.current as number);
+        setTimeOutRef.current = null;
+      }, timeoutMs || 0);
+    },
+    [setTimeOutRef]
+  );
   const fetchData = () => {
     axios
-      .request(axiosParams)
+      .request({ ...axiosParams, signal: /**this is for cancelling the request */ abortControllerRef.current.signal /* we can also use AbortSignal.timeout(timeout) to timeout the request*/ })
       .then((res: AxiosResponse) => {
         setResponse(res.data);
       })
       .catch((err) => {
-        setError(err);
+        // this will narrow the type :)
+        // Access to config, request, and response
+        if (axios.isAxiosError(err)) {
+          if (err.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          } else if (err.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(err.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", err.message);
+          }
+          console.log(err.config);
+          setError(err.toJSON());
+        } else {
+          setError(err.toJson());
+          // Just a stock error
+        }
       })
       .finally(() => {
         setLoading(false);
@@ -27,5 +76,5 @@ export const useAxios = <T,>(axiosParams: AxiosRequestConfig) => {
     fetchData();
   }, []);
   // custom hook returns value
-  return { response, error, loading };
+  return { response, error, loading, cancelRequest };
 };
